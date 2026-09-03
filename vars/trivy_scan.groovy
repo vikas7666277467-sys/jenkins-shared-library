8eed51dd945bc708c60b36d4fb0b44a591b0a8b7
Vikas Mishra <vikasmishra40423@gmail.com>
2026-09-03 11:34:58 +0530

    Fix Trivy path for Windows Jenkins

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,6 +1,6 @@
 def call() {
     bat """
-        trivy image --exit-code 0 --severity HIGH,CRITICAL ${env.DOCKER_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
-        trivy image --exit-code 0 --severity HIGH,CRITICAL ${env.DOCKER_MIGRATION_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
+        "C:\\Tools\\Trivy\\trivy.exe" image --exit-code 0 --severity HIGH,CRITICAL ${env.DOCKER_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
+        "C:\\Tools\\Trivy\\trivy.exe" image --exit-code 0 --severity HIGH,CRITICAL ${env.DOCKER_MIGRATION_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
     """
 }
