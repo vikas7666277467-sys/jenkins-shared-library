620c7f8e8c9d7bb6b30ce2d972c5de5d23a305cd
Vikas Mishra <vikasmishra40423@gmail.com>
2026-09-21 08:31:48 +0530

    Fix Docker Hub credential diagnostics

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     def imageName = config.imageName
     def imageTag = config.imageTag
-    def credentialsId = config.credentials ?: 'docker-hub-credentials'
+    def credentialsId = config.credentials ?: 'dockerhub-creds'
 
     withCredentials([usernamePassword(
         credentialsId: credentialsId,
@@ -10,7 +10,20 @@ def call(Map config) {
     )]) {
 
         powershell '''
+            $ErrorActionPreference = "Stop"
+
+            Write-Host "Docker username: $env:DOCKER_USERNAME"
+            Write-Host "Docker password received: $([bool]$env:DOCKER_PASSWORD)"
+            Write-Host "Docker password length: $($env:DOCKER_PASSWORD.Length)"
+
             $env:DOCKER_PASSWORD | docker login -u $env:DOCKER_USERNAME --password-stdin
+
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "Docker login failed"
+                exit $LASTEXITCODE
+            }
+
+            Write-Host "Docker login successful"
         '''
 
         bat """
